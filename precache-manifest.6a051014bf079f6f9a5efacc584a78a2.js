c44109528d487c9dc32bcfd530d0831e2f0cf588
Marcus Wassén <marcus.wassen@fbinhouse.se>
2020-01-29 15:13:30 +0100

    Updates

---
+++ precache-manifest.6a051014bf079f6f9a5efacc584a78a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd9af1a4c87507c8aecca5ffdfc9985",
+    "url": "/music-forecast/index.html"
+  },
+  {
+    "revision": "f2674544ae93b06dbd1d",
+    "url": "/music-forecast/static/css/main.c8cc2176.chunk.css"
+  },
+  {
+    "revision": "65997cf7c0fd040c92b8",
+    "url": "/music-forecast/static/js/2.789f543e.chunk.js"
+  },
+  {
+    "revision": "bc606d77c80b361b8c716f47e55fb719",
+    "url": "/music-forecast/static/js/2.789f543e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f2674544ae93b06dbd1d",
+    "url": "/music-forecast/static/js/main.9f232f9b.chunk.js"
+  },
+  {
+    "revision": "895f9951a5526226b524",
+    "url": "/music-forecast/static/js/runtime-main.c74cf792.js"
+  },
+  {
+    "revision": "5d762816c4ea151c7ed35814206082b9",
+    "url": "/music-forecast/static/media/logo.5d762816.svg"
+  }
+]);
